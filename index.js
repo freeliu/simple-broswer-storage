070bce825e19792e420347acd902a86736cbe31c
olive <liurc@outlook.com>
2018-05-23 14:03:54 +0800

    add expried time

---
+++ index.js
@@ -0,0 +1,34 @@
+let sessionTimer = {}
+let localTimer = {}
+let storage = {
+
+}
+/**
+ * @param {string} name sessionStorage name
+ * @param {string} value sessionStorage value
+ * @param {number} expried expried in how many millisecond
+ */
+export function sessionSet(name, value, expried) {
+    sessionStorage.name = value
+    if (sessionTimer.name) {
+        clearTimeout(sessionTimer.name);
+    }
+    sessionTimer.name = setTimeout(() => {
+        sessionStorage.removeItem(name)
+    }, expried)
+}
+
+/**
+ * @param {string} name sessionStorage name
+ * @param {string} value sessionStorage value
+ * @param {number} expried expried in how many millisecond
+ */
+export function localSet(name, value, expried) {
+    localStorage.name = value
+    if (localTimer.name) {
+        clearTimeout(localTimer.name);
+    }
+    localTimer.name = setTimeout(() => {
+        localStorage.removeItem(name)
+    }, expried)
+}
